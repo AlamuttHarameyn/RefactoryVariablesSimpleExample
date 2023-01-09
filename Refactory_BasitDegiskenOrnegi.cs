83f6626fdd4aed8e882de13d136e540862e1f958
Gizem <gizemustahuseyin123@gmail.com>
2023-01-09 19:38:01 +0300

    Add files via upload

---
+++ Refactory_BasitDegiskenOrnegi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._11._1_KarakterDizilimleri
+{
+    class Refactory_BasitDegiskenOrnegi
+    {
+        static void Main(string[] args)
+        {
+            //Kodu iyileştirmeye refactory denir.
+            //hızlı işlemler adlı menüden bu işlemleri kolayca yapabiliriz
+            //örneğin kodu methoda dönüştürmek için kullanabiliriz.
+            //kodu  methoda çevirmek için ctrl+. tuşlarına basarak küçük bir menü açıp ardından
+            //extract methodu seçiyoruz.yeniden adlandırıyoruz.kodumuzu methoda çevrildi.
+
+            int sayi = 2;
+            Console.WriteLine(sayi); //Sayi 2 demek
+            Console.WriteLine(sayi + 2); //sayiya 2 ekle
+            Console.WriteLine(sayi * sayi); //sayiyi sayiyla carp
+            Console.WriteLine(sayi - 5); //sayidan 5 çıkar
+
+            Console.ReadKey();
+        }
+    }
+}
